a3a9ffdda3f2c48bee0eae3e357b5d5d8c28d343
Michal_Marszalek <1aam2am1@gmail.com>
2020-04-26 23:05:44 +0200

    Created basic Client Implementation.

--- SmartControl/Api/Client.cs
+++ SmartControl/Api/Client.cs
@@ -1,8 +1,12 @@
 ﻿using SmartControl.Api.Data;
 using SmartControl.Api.Server;
+using SmartControl.Api.Server.Queries;
+using SmartControl.Api.Server.Responses;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SmartControl.Api
@@ -15,6 +19,9 @@ namespace SmartControl.Api
         //public Lazy<IServer> server = new Lazy<IServer>(new HttpServer());
         public Lazy<IServer> server = new Lazy<IServer>(new FileServer());
 
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private readonly object locker = new object();
+
         /// <summary>
         /// Connect to server with given settings
         /// </summary>
@@ -23,10 +30,179 @@ namespace SmartControl.Api
         /// <param name="onConnection">1 when OK 0 if error</param>
         async public void Connect(ConnectSettings s, Credentials i, Action<bool> onConnection)
         {
-            var task = await server.Value.Auth(s, i);
-            //wait for it to end without blocking the main thread
+            try
+            {
+                cancellationTokenSource.Cancel();
+                var task = await server.Value.Auth(s, i);
+                //wait for it to end without blocking the main thread
 
-            onConnection?.Invoke(task);
+                onConnection?.Invoke(task);
+            }
+            catch
+            {
+                onConnection?.Invoke(false);
+            }
         }
+
+        private readonly DataManager data = new DataManager();
+        public DataManager GetDataManager()
+        {
+            return data;
+        }
+
+        /// <summary>
+        /// Function start data synchronization with connected device.
+        /// </summary>
+        public void StartDataSynchronization()
+        {
+            server.Value.StartAsyncPing(PingAction);
+        }
+
+        public void StopDataSynchronization()
+        {
+            server.Value.ClosePingClient();
+        }
+
+        #region Internal
+
+        private async void PingAction(int action)
+        {
+            try
+            {
+                switch (action)
+                {
+                    case -1:
+                        StopDataSynchronization();
+                        await Task.Delay(1000);
+                        StartDataSynchronization();
+                        break;
+                    case 0:
+                        //no change
+                        break;
+                    case 1:
+                        //change
+                        await ResolveStatusChange();
+                        break;
+                }
+            }
+            catch (AggregateException e)
+            {
+                foreach (var v in e.InnerExceptions)
+                {
+                    if (v.Message.Length == 0)
+                    {
+                        //connection error re try
+                        StopDataSynchronization();
+                        await Task.Delay(1000);
+                        StartDataSynchronization();
+                    }
+                    else
+                    {
+                        //message from server error
+                        Console.WriteLine("Exception: {0}", v.Message);
+                    }
+                }
+            }
+            catch
+            {
+                //some internal error
+            }
+        }
+
+        public async Task ResolveStatusChange()
+        {
+            var response = await server.Value.GetFullStatus();
+            List<Task> waitfor = new List<Task>();
+
+            if (response.ChangedParameters)
+            {
+                waitfor.Add(ResolveParametersChange());
+            }
+            if (response.ChangedCalendar)
+            {
+                waitfor.Add(ResolveCalendarChange());
+            }
+            if (response.ChangedMod)
+            {
+                waitfor.Add(ResolveModChange());
+            }
+
+            lock (locker)
+            {
+                data.BateryLow = response.BateryLow;
+                data.ByPass = response.ByPass;
+                data.Errors = response.Errors;
+                data.Heater = response.Heater;
+                data.Status = response.Status;
+                data.Work = response.Work;
+            }
+
+            Task.WaitAll(waitfor.ToArray());
+        }
+
+        public async Task ResolveParametersChange()
+        {
+            var query = new ParameterQuery();
+            query.Parameter.AddRange(Enumerable.Range(0, 17));
+            query.Parameter.AddRange(Enumerable.Range(20, 60));
+            query.Parameter.Add(255);
+
+            var parameters = await server.Value.GetParameters(query);
+            lock (locker)
+            {
+                data.UpdateParameters(parameters.Parameters);
+            }
+        }
+
+        public async Task ResolveCalendarChange()
+        {
+            List<Task> waitfor = new List<Task>();
+
+            var d = server.Value.GetCalendarData();
+
+            foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                waitfor.Add(ResolveTaskChange(day));
+            }
+
+            var result = await d;
+
+            lock (locker)
+            {
+                data.ActiveDays = result.ActiveDays;
+                data.CalState = result.CalState;
+                data.CalDate = result.Date;
+                data.CalEnabled = result.Enabled;
+            }
+
+            Task.WaitAll(waitfor.ToArray());
+        }
+
+        public async Task ResolveTaskChange(DayOfWeek day)
+        {
+            var query = new CalendarDayQuery
+            {
+                Day = day
+            };
+
+            var parametres = await server.Value.GetTask(query);
+
+            lock (locker)
+            {
+                data.UpdateTask(parametres.Day, parametres.Tasks);
+            }
+        }
+
+        public async Task ResolveModChange()
+        {
+            var result = await server.Value.GetModes();
+
+            lock (locker)
+            {
+                data.UpdateModes(result.Modes);
+            }
+        }
+
+        #endregion
     }
 }
--- SmartControl/Api/Data/DataManager.cs
+++ SmartControl/Api/Data/DataManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Collections.ObjectModel;
 
 namespace SmartControl.Api.Data
 {
@@ -10,8 +11,170 @@ namespace SmartControl.Api.Data
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        #region Parameters
+        private readonly Dictionary<int, int> _Parameters = new Dictionary<int, int>();
+        public ReadOnlyDictionary<int, int> Parameters
+        {
+            get => new ReadOnlyDictionary<int, int>(_Parameters);
+        }
+        public void UpdateParameters(Dictionary<int, int> value)
+        {
+            foreach (KeyValuePair<int, int> item in value)
+            {
+                _Parameters[item.Key] = item.Value;
+            }
+            NotifyPropertyChanged("Parameters");
+        }
+        #endregion
 
+        #region Task
+        private readonly Dictionary<DayOfWeek, List<CalendarTask>> _Task = new Dictionary<DayOfWeek, List<CalendarTask>>();
+        public ReadOnlyDictionary<DayOfWeek, List<CalendarTask>> Task
+        {
+            get => new ReadOnlyDictionary<DayOfWeek, List<CalendarTask>>(_Task);
+        }
+        public void UpdateTask(DayOfWeek day, List<CalendarTask> list)
+        {
+            _Task[day] = list;
+            NotifyPropertyChanged("Task");
+        }
+        #endregion
 
+        #region Calendar
+        int _ActiveDays;
+        public int ActiveDays
+        {
+            get => _ActiveDays;
+            set
+            {
+                _ActiveDays = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        int _CalState;
+        public int CalState
+        {
+            get => _CalState;
+            set
+            {
+                _CalState = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        DateTime _CalDate;
+        public DateTime CalDate
+        {
+            get => _CalDate;
+            set
+            {
+                _CalDate = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        bool _CalEnabled;
+        public bool CalEnabled
+        {
+            get => _CalEnabled;
+            set
+            {
+                _CalEnabled = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Modes
+        private readonly Dictionary<int, ModesStatus> _Modes = new Dictionary<int, ModesStatus>();
+        public ReadOnlyDictionary<int, ModesStatus> Modes
+        {
+            get => new ReadOnlyDictionary<int, ModesStatus>(_Modes);
+        }
+        public void UpdateModes(Dictionary<int, ModesStatus> value)
+        {
+            foreach (KeyValuePair<int, ModesStatus> item in value)
+            {
+                _Modes[item.Key] = item.Value;
+            }
+            NotifyPropertyChanged("Modes");
+        }
+        #endregion
+
+        #region Status
+
+        WorkStatus _Status;
+        public WorkStatus Status
+        {
+            get => _Status;
+            set
+            {
+                _Status = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        ModStatus _Work;
+        public ModStatus Work
+        {
+            get => Work;
+            set
+            {
+                _Work = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        bool _Heater;
+        public bool Heater
+        {
+            get => _Heater;
+            set
+            {
+                _Heater = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        bool _ByPass;
+        public bool ByPass
+        {
+            get => _ByPass;
+            set
+            {
+                _ByPass = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        int _Errors;
+        /// <summary>
+        /// Errors and parts of calendar
+        /// </summary>
+        public int Errors
+        {
+            get => _Errors;
+            set
+            {
+                _Errors = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        bool _BateryLow;
+        public bool BateryLow
+        {
+            get => _BateryLow;
+            set
+            {
+                _BateryLow = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
 
         void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
         {
